0003491c0a0e5eb86d6bfb6a0b5d9ec0ac44a4b8
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-07-18 02:45:16 +0000

    1.把驱动默认添加到框架中来； 2.增强原有的点击操作；

--- example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/selenium/SeleniumClick.java
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/selenium/SeleniumClick.java
@@ -3,6 +3,7 @@
 */
 package org.suren.autotest.web.framework.selenium;
 
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,22 +27,37 @@ public class SeleniumClick implements ClickAble
 	@Autowired
 	private SearchStrategyUtils		searchStrategyUtils;
 
+	@Override
 	public void click(Element ele)
 	{
-		searchStrategyUtils.findStrategy(WebElement.class, ele).search(ele).click();
+		WebElement webEle = searchStrategyUtils.findStrategy(WebElement.class, ele).search(ele);
+		try
+		{
+			webEle.click();
+		}
+		catch(WebDriverException e)
+		{
+			if(e.getMessage().contains("Element is not clickable at point"))
+			{
+				new Actions(engine.getDriver()).click(webEle);
+			}
+		}
 	}
 
+	@Override
 	public void dbClick(Element ele)
 	{
 		Actions actions = new Actions(engine.getDriver());
 		actions.doubleClick(searchStrategyUtils.findStrategy(WebElement.class, ele).search(ele));
 	}
 
+	@Override
 	public boolean isEnabled(Element element)
 	{
 		return searchStrategyUtils.findStrategy(WebElement.class, element).search(element).isEnabled();
 	}
 
+	@Override
 	public boolean isHidden(Element element)
 	{
 		return !searchStrategyUtils.findStrategy(WebElement.class, element).search(element).isDisplayed();

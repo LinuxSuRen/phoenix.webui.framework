ae756a0674b7090034f72414e4f31c8804df2e41
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-07-23 09:10:42 +0000

    结构整理

--- src/main/java/org/suren/autotest/web/framework/validation/Validation.java
+++ src/main/java/org/suren/autotest/web/framework/validation/Validation.java
@@ -3,7 +3,9 @@
  */
 package org.suren.autotest.web.framework.validation;
 
+import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.net.URL;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
@@ -21,11 +23,19 @@ public class Validation
 	public static void validation(String xsdFile, InputStream xmlInput) throws Exception
 	{
 		SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
-		Schema schema = factory.newSchema(Validation.class.getClassLoader().getResource(xsdFile));
-		Validator validator = schema.newValidator();
-		
-		Source source = new StreamSource(xmlInput);
-		validator.validate(source);
+		URL xsdURL = Validation.class.getClassLoader().getResource(xsdFile);
+		if(xsdURL != null)
+		{
+			Schema schema = factory.newSchema(xsdURL);
+			Validator validator = schema.newValidator();
+			
+			Source source = new StreamSource(xmlInput);
+			validator.validate(source);
+		}
+		else
+		{
+			throw new FileNotFoundException(String.format("can not found xsd file [%s] from classpath.", xsdFile));
+		}
 	}
 	
 	public static void validationFramework(InputStream xmlInput) throws Exception

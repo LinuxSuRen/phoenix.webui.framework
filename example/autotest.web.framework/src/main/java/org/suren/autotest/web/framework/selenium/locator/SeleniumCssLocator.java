f11f443dc7d14b7d4d772b228a0bf4e551a2c331
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-07-25 05:40:25 +0000

    1.循环策略调试完毕； 2.IFrame调试完毕；

--- example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/selenium/locator/SeleniumCssLocator.java
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/selenium/locator/SeleniumCssLocator.java
@@ -6,8 +6,6 @@ package org.suren.autotest.web.framework.selenium.locator;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import org.suren.autotest.web.framework.core.Locator;
-import org.suren.autotest.web.framework.core.LocatorAware;
 
 /**
  * @author suren
@@ -15,28 +13,11 @@ import org.suren.autotest.web.framework.core.LocatorAware;
  */
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-public class SeleniumCssLocator implements Locator, LocatorAware
+public class SeleniumCssLocator extends AbstractLocator
 {
-
 	@Override
 	public String getType()
 	{
-		// TODO Auto-generated method stub
 		return "byCss";
 	}
-
-	@Override
-	public String getValue()
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setValue(String value)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
 }
--- example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/selenium/strategy/ZoneSearchStrategy.java
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/selenium/strategy/ZoneSearchStrategy.java
@@ -3,7 +3,11 @@ package org.suren.autotest.web.framework.selenium.strategy;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.suren.autotest.web.framework.core.ElementSearchStrategy;
@@ -22,7 +26,8 @@ import org.suren.autotest.web.framework.selenium.SeleniumEngine;
 @Component("zoneSearchStrategy")
 public class ZoneSearchStrategy implements ElementSearchStrategy<WebElement>
 {
-
+	private static final Logger logger = LoggerFactory.getLogger(ZoneSearchStrategy.class);
+	
 	@Autowired
 	private SeleniumEngine engine;
 
@@ -36,13 +41,27 @@ public class ZoneSearchStrategy implements ElementSearchStrategy<WebElement>
 			return webEle;
 		}
 		
+		logger.info(String.format("zone search strategy, locators count[%s].", locators.size()));
+		
+		WebDriver driver = engine.getDriver();
 		for(Locator locator : locators)
 		{
 			By by = null;
 			String type = locator.getType();
+			String value = locator.getValue();
 			if("byId".equals(type))
 			{
-				by = By.id(locator.getValue());
+				by = By.id(value);
+			}
+			else if("byIFrame".equals(type))
+			{
+				driver = driver.switchTo().frame(value);
+				
+				logger.debug(String.format("iframe[%s] siwtch success.", value));
+			}
+			else if("byLinkText".equals(type))
+			{
+				by = By.linkText(value);
 			}
 			
 			if(by == null)
@@ -50,13 +69,23 @@ public class ZoneSearchStrategy implements ElementSearchStrategy<WebElement>
 				continue;
 			}
 			
-			if(webEle == null)
+			try
 			{
-				webEle = engine.getDriver().findElement(by);
+				if(webEle == null)
+				{
+					webEle = driver.findElement(by);
+				}
+				else
+				{
+					webEle = webEle.findElement(by);
+				}
 			}
-			else
+			catch(NoSuchElementException e)
 			{
-				webEle = webEle.findElement(by);
+				logger.error(
+						String.format("can not found element by[%s]", by), e);
+				
+				break;
 			}
 		}
 		

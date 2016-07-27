db7977caadcf25dd4d7390d990068cd0da97c33a
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-07-27 04:55:42 +0000

    添加注解

--- src/main/java/org/suren/autotest/web/framework/selenium/locator/SeleniumTextLocator.java
+++ src/main/java/org/suren/autotest/web/framework/selenium/locator/SeleniumTextLocator.java
@@ -3,10 +3,16 @@
  */
 package org.suren.autotest.web.framework.selenium.locator;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 /**
  * @author suren
  * @date 2016年7月27日 下午12:11:23
  */
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SeleniumTextLocator extends AbstractLocator
 {
 

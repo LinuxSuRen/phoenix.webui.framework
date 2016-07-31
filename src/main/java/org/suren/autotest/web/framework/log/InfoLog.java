293415a52ac6175440965c95eabd004024e95342
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-07-31 05:50:44 +0000

    切面日志调试完成

--- src/main/java/org/suren/autotest/web/framework/log/InfoLog.java
+++ src/main/java/org/suren/autotest/web/framework/log/InfoLog.java
@@ -3,31 +3,52 @@
  */
 package org.suren.autotest.web.framework.log;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * @author suren
  * @date Jul 30, 2016 10:09:16 PM
  */
+@Component
 @Aspect
 public class InfoLog
 {
-	public InfoLog()
+	private static final Logger logger = LoggerFactory.getLogger(InfoLog.class);
+	
+//	@Before("execution(* org.suren.autotest.web.framework.core.ElementSearchStrategy.search*(..))")
+	public void before()
 	{
-		System.out.println("info log.");
+		System.out.println("hell");
 	}
 	
-	@Pointcut("execution(* org.suren.autotest.web.framework..*(..))")
-	public void test()
+	@Around("execution(* org.suren.autotest.web.framework.core.ElementSearchStrategy.search*(..))")
+	public Object hello(ProceedingJoinPoint joinPoint)
 	{
-		System.out.println("test");
-	}
-	
-	public void hello(JoinPoint point)
-	{
-		System.out.println("hello");
+		long beginTime = System.currentTimeMillis();
+		
+		Object[] args = joinPoint.getArgs();
+		
+		Object res = null;
+		try
+		{
+			res = joinPoint.proceed(args);
+		}
+		catch (Throwable e)
+		{
+			e.printStackTrace();
+		}
+		
+		long endTime = System.currentTimeMillis();
+		
+		logger.debug(String.format("target [%s] time [%s].",
+				joinPoint.getSignature(), endTime - beginTime));
+		
+		return res;
 	}
 }
--- src/main/java/org/suren/autotest/web/framework/selenium/action/SeleniumCheck.java
+++ src/main/java/org/suren/autotest/web/framework/selenium/action/SeleniumCheck.java
@@ -9,8 +9,12 @@ import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+import org.suren.autotest.web.framework.core.ElementSearchStrategy;
 import org.suren.autotest.web.framework.core.Locator;
 import org.suren.autotest.web.framework.core.action.CheckAble;
 import org.suren.autotest.web.framework.core.ui.Element;
@@ -23,7 +27,7 @@ import org.suren.autotest.web.framework.selenium.strategy.ZoneSearchStrategy;
  * @date Jul 27, 2016 1:00:30 PM
  */
 @Component
-public class SeleniumCheck implements CheckAble
+public class SeleniumCheck implements CheckAble, ApplicationContextAware
 {
 	private static final Logger logger = LoggerFactory.getLogger(SeleniumCheck.class);
 	
@@ -31,9 +35,10 @@ public class SeleniumCheck implements CheckAble
 	private SearchStrategyUtils		searchStrategyUtils;
 	
 	@Autowired
-	private SeleniumValueLocator valueLocator;
-	@Autowired
-	private ZoneSearchStrategy zoneSearchStrategy;
+//	private SeleniumValueLocator valueLocator;
+//	@Autowired
+//	private ZoneSearchStrategy zoneSearchStrategy;
+	private ApplicationContext context;
 
 	@Override
 	public void checkByText(Element element, String text)
@@ -48,6 +53,9 @@ public class SeleniumCheck implements CheckAble
 		List<Locator> locatorList = element.getLocatorList();
 		List<Locator> tmpList = new ArrayList<Locator>(locatorList);
 		
+		ElementSearchStrategy<WebElement> strategy = context.getBean("zoneSearchStrategy", ElementSearchStrategy.class);
+		
+		SeleniumValueLocator valueLocator = context.getBean(SeleniumValueLocator.class);
 		valueLocator.setHostType("byTagName");
 		valueLocator.setHostValue("input");
 		
@@ -57,7 +65,7 @@ public class SeleniumCheck implements CheckAble
 		
 		try
 		{
-			WebElement itemWebEle = zoneSearchStrategy.search(element);
+			WebElement itemWebEle = strategy.search(element);
 			if(itemWebEle != null)
 			{
 				itemWebEle.click();
@@ -68,6 +76,13 @@ public class SeleniumCheck implements CheckAble
 			locatorList.clear();
 			locatorList.addAll(tmpList);
 		}
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException
+	{
+		this.context = applicationContext;
 	}
 
 }

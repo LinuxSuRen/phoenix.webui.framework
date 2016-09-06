3d94a9754e2eb5f2e6315272a14debd42cc5adaa
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-09-06 23:52:09 +0000

    增加读写测试报告的接口

---
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/report/RecordReportReader.java
@@ -0,0 +1,14 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.report;
+
+/**
+ * 记录报告读取
+ * @author suren
+ * @date 2016年9月6日 下午8:45:45
+ */
+public interface RecordReportReader
+{
+
+}

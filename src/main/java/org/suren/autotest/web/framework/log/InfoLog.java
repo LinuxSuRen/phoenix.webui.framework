39cea0635f96a0139e1eb254fbb70f40da1832df
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-09-06 23:55:35 +0000

    增加日志类型的测试报告

--- src/main/java/org/suren/autotest/web/framework/log/InfoLog.java
+++ src/main/java/org/suren/autotest/web/framework/log/InfoLog.java
@@ -8,7 +8,11 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.suren.autotest.web.framework.report.RecordReportWriter;
+import org.suren.autotest.web.framework.report.record.ExceptionRecord;
+import org.suren.autotest.web.framework.report.record.SearchRecord;
 
 /**
  * 有关元素查找所需时间的日志输出
@@ -21,6 +25,9 @@ public class InfoLog
 {
 	private static final Logger LOGGER = LoggerFactory.getLogger(InfoLog.class);
 	
+	@Autowired
+	private RecordReportWriter recordReportWriter;
+	
 	@Around("execution(* org.suren.autotest.web.framework.core.action.ClickAble.click*(..))")
 	public Object clickAround(ProceedingJoinPoint joinPoint) throws Throwable
 	{
@@ -30,7 +37,7 @@ public class InfoLog
 	}
 	
 	@Around("execution(* org.suren.autotest.web.framework.core.ElementSearchStrategy.search*(..))")
-	public Object hello(ProceedingJoinPoint joinPoint) throws Throwable
+	public Object searchLog(ProceedingJoinPoint joinPoint) throws Throwable
 	{
 		long beginTime = System.currentTimeMillis();
 		
@@ -39,10 +46,28 @@ public class InfoLog
 		Object res = joinPoint.proceed(args);
 		
 		long endTime = System.currentTimeMillis();
+		long cost = endTime - beginTime;
 		
-		LOGGER.debug(String.format("target [%s] time [%s].",
-				joinPoint.getSignature(), endTime - beginTime));
+		ExceptionRecord exceptionRecord = new SearchRecord(cost);
+		exceptionRecord.setMessage(joinPoint.getSignature().toLongString());
+		recordReportWriter.write(exceptionRecord);
 		
 		return res;
+	}
+
+	/**
+	 * @return the recordReportWriter
+	 */
+	public RecordReportWriter getRecordReportWriter()
+	{
+		return recordReportWriter;
+	}
+
+	/**
+	 * @param recordReportWriter the recordReportWriter to set
+	 */
+	public void setRecordReportWriter(RecordReportWriter recordReportWriter)
+	{
+		this.recordReportWriter = recordReportWriter;
 	}
 }

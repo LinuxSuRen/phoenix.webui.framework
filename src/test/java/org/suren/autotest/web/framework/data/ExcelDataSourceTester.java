11b697cb465095c857d583dd20d8b4d095b401be
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-09-06 23:57:22 +0000

    类的包修改

---
+++ src/test/java/org/suren/autotest/web/framework/data/ExcelDataSourceTester.java
@@ -0,0 +1,22 @@
+package org.suren.autotest.web.framework.data;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.suren.autotest.web.framework.data.ExcelDataSource;
+import org.suren.autotest.web.framework.data.FileResource;
+
+/**
+ * excel测试类
+ * @author suren
+ * @date 2016年9月6日 下午9:27:07
+ */
+public class ExcelDataSourceTester {
+
+	@Test
+	public void excelDataSource() {
+		ExcelDataSource excelDataSource = new ExcelDataSource();
+		
+		excelDataSource.loadData(new FileResource(new File("D:/b.xlsx")), null);
+	}
+}

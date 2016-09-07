ca8dec1c5ac44129376ded9f28280df8b649d98b
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-09-07 23:49:45 +0000

    增加用于描述测试套件的类

---
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/core/suite/SuiteAction.java
@@ -0,0 +1,87 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.core.suite;
+
+/**
+ * 测试套件中的动作定义
+ * @author suren
+ * @date 2016年9月7日 下午10:00:49
+ */
+public class SuiteAction
+{
+	/** 属性名 */
+	private String field;
+	/** 动作名 */
+	private String name;
+	/** 操作前休眠时间 */
+	private long beforeSleep;
+	/** 操作后休眠时间 */
+	private long afterSleep;
+	public SuiteAction(){}
+	/**
+	 * @param field
+	 * @param name
+	 */
+	public SuiteAction(String field, String name)
+	{
+		this.field = field;
+		this.name = name;
+	}
+	/**
+	 * @return the field
+	 */
+	public String getField()
+	{
+		return field;
+	}
+	/**
+	 * @param field the field to set
+	 */
+	public void setField(String field)
+	{
+		this.field = field;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	/**
+	 * @return the beforeSleep
+	 */
+	public long getBeforeSleep()
+	{
+		return beforeSleep;
+	}
+	/**
+	 * @param beforeSleep the beforeSleep to set
+	 */
+	public void setBeforeSleep(long beforeSleep)
+	{
+		this.beforeSleep = beforeSleep;
+	}
+	/**
+	 * @return the afterSleep
+	 */
+	public long getAfterSleep()
+	{
+		return afterSleep;
+	}
+	/**
+	 * @param afterSleep the afterSleep to set
+	 */
+	public void setAfterSleep(long afterSleep)
+	{
+		this.afterSleep = afterSleep;
+	}
+}

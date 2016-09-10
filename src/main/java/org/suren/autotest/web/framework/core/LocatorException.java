87b1d6063e10f719fb83d49e60e63f5d87aa9c2f
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-09-10 23:49:56 +0000

    增加元素定位器异常

---
+++ src/main/java/org/suren/autotest/web/framework/core/LocatorException.java
@@ -0,0 +1,17 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.core;
+
+/**
+ * 元素定位器异常
+ * @author suren
+ * @date 2016年9月10日 下午7:49:33
+ */
+public class LocatorException extends RuntimeException
+{
+
+	/**  */
+	private static final long	serialVersionUID	= 1L;
+
+}
---
+++ src/main/java/org/suren/autotest/web/framework/core/LocatorNotFoundException.java
@@ -0,0 +1,17 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.core;
+
+/**
+ * 元素定位器找不到异常
+ * @author suren
+ * @date 2016年9月10日 下午7:50:29
+ */
+public class LocatorNotFoundException extends LocatorException
+{
+
+	/**  */
+	private static final long	serialVersionUID	= 1L;
+
+}

c5660609a99eae249a9adb1ffcf17db1d4d45f0d
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-11-24 12:51:27 +0000

    增加其他浏览器的常量

--- src/main/java/org/suren/autotest/web/framework/settings/DriverConstants.java
+++ src/main/java/org/suren/autotest/web/framework/settings/DriverConstants.java
@@ -5,14 +5,17 @@ package org.suren.autotest.web.framework.settings;
 
 /**
  * 支持的浏览器引擎常量
+ * 
  * @author suren
  * @date Jul 17, 2016 9:33:30 AM
  */
 public interface DriverConstants
 {
-	String DRIVER_CHROME = "chrome";
-	String DRIVER_IE = "ie";
-	String DRIVER_FIREFOX = "firefox";
-	
-	String ENGINE_CONFIG_FILE_NAME = "engine.properties";
+	String	DRIVER_CHROME			= "chrome";
+	String	DRIVER_IE				= "ie";
+	String	DRIVER_FIREFOX			= "firefox";
+	String	DRIVER_SAFARI			= "safari";
+	String	DRIVER_OPERA			= "opera";
+
+	String	ENGINE_CONFIG_FILE_NAME	= "engine.properties";
 }

6b1080fdf310d419b478cb7e7b14ca06d5547385
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-11-24 00:27:07 +0000

    增加异常定义

---
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/core/ConfigNotFoundException.java
@@ -0,0 +1,33 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.core;
+
+/**
+ * 找不到配置文件异常
+ * @author suren
+ * @date 2016年11月24日 上午8:22:50
+ */
+public class ConfigNotFoundException extends AutoTestException
+{
+
+	/**  */
+	private static final long	serialVersionUID	= 1L;
+
+	private String type;
+	private String configPath;
+	
+	public ConfigNotFoundException(String type, String configPath)
+	{
+		this.type = type;
+		this.configPath = configPath;
+	}
+
+	@Override
+	public String getMessage()
+	{
+		return String.format("Can not found config file for AutoTest,"
+				+ " type [%s], path [%s].", type, configPath);
+	}
+
+}

8454b956445c58ea0f2ef2544a923496f0dd6d17
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-11-24 00:27:07 +0000

    增加异常定义

---
+++ src/main/java/org/suren/autotest/web/framework/core/AutoTestException.java
@@ -0,0 +1,17 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.core;
+
+/**
+ * Auto框架的顶级异常类
+ * @author suren
+ * @date 2016年11月24日 上午8:20:49
+ */
+public class AutoTestException extends RuntimeException
+{
+
+	/**  */
+	private static final long	serialVersionUID	= 1L;
+
+}

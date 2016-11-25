c45fabd71dda09acb9a1b936a00da682ebbb8b86
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-11-25 00:08:39 +0000

    增加鼠标和键盘的接口

---
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/core/Keyboard.java
@@ -0,0 +1,21 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.core;
+
+/**
+ * 操作键盘的接口
+ * @author suren
+ * @date 2016年11月25日 上午8:05:57
+ */
+public interface Keyboard
+{
+	/**
+	 * 按下回车键
+	 */
+	void enter();
+	/**
+	 * 按下空格键
+	 */
+	void space();
+}

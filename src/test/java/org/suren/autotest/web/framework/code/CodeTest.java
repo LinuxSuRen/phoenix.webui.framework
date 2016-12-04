f16bdcda343bdc01587c11459782831d85b738a3
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-12-04 01:47:18 +0000

    freemarker测试代码

---
+++ src/test/java/org/suren/autotest/web/framework/code/CodeTest.java
@@ -0,0 +1,21 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.code;
+
+/**
+ * @author suren
+ * @date 2016年12月3日 下午8:59:07
+ */
+public class CodeTest
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		new DefaultXmlCodeGenerator().generate("ad.xml");
+	}
+
+}

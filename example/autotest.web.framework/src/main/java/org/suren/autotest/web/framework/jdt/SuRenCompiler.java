ceb344a66e1c7a61f33ed2109f7507c7c6408da4
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-12-04 08:18:16 +0000

    完善编译功能

--- example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/jdt/SuRenCompiler.java
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/jdt/SuRenCompiler.java
@@ -3,33 +3,56 @@
  */
 package org.suren.autotest.web.framework.jdt;
 
-import org.eclipse.jdt.internal.compiler.Compiler;
-import org.eclipse.jdt.internal.compiler.ICompilerRequestor;
-import org.eclipse.jdt.internal.compiler.IErrorHandlingPolicy;
-import org.eclipse.jdt.internal.compiler.IProblemFactory;
-import org.eclipse.jdt.internal.compiler.env.INameEnvironment;
-import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jdt.internal.compiler.env.ICompilationUnit;
 
 /**
  * @author suren
  * @date 2016年11月26日 下午5:52:59
  */
-public class SuRenCompiler extends Compiler
+public class SuRenCompiler implements ICompilationUnit
 {
+    private File file;
+    private String workDir;
 
-	/**
-	 * @param environment
-	 * @param policy
-	 * @param options
-	 * @param requestor
-	 * @param problemFactory
-	 */
-	public SuRenCompiler(INameEnvironment environment,
-			IErrorHandlingPolicy policy, CompilerOptions options,
-			ICompilerRequestor requestor, IProblemFactory problemFactory)
-	{
-		super(environment, policy, options, requestor, problemFactory);
-		// TODO Auto-generated constructor stub
-	}
+    public SuRenCompiler(File file, String workDir) {
+        this.file = file;
+        this.workDir = workDir;
+    }
 
+    public char[] getContents() {
+        try {
+            return FileUtils.readFileToString(file).toCharArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public char[] getMainTypeName() {
+        return file.getName().replace(".java", "").toCharArray();
+    }
+
+    public char[][] getPackageName() {
+        String fullPkgName = this.file.getParentFile().getAbsolutePath().replace(workDir, "");
+        fullPkgName = fullPkgName.replace("/", ".").replace("\\", ".");
+        if (fullPkgName.startsWith("."))
+            fullPkgName = fullPkgName.substring(1);
+        String[] items = fullPkgName.split("[.]");
+        char[][] pkgName = new char[items.length][];
+        for (int i = 0; i < items.length; i++) {
+            pkgName[i] = items[i].toCharArray();
+        }
+        return pkgName;
+    }
+
+    public boolean ignoreOptionalProblems() {
+        return false;
+    }
+
+    public char[] getFileName() {
+        return this.file.getName().toCharArray();
+    }
 }
--- example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/jdt/SuRenCompilerRequestor.java
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/jdt/SuRenCompilerRequestor.java
@@ -3,6 +3,12 @@
  */
 package org.suren.autotest.web.framework.jdt;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jdt.core.compiler.IProblem;
+import org.eclipse.jdt.internal.compiler.ClassFile;
 import org.eclipse.jdt.internal.compiler.CompilationResult;
 import org.eclipse.jdt.internal.compiler.ICompilerRequestor;
 
@@ -12,12 +18,49 @@ import org.eclipse.jdt.internal.compiler.ICompilerRequestor;
  */
 public class SuRenCompilerRequestor implements ICompilerRequestor
 {
-
-	@Override
-	public void acceptResult(CompilationResult arg0)
+	private String workDir;
+	
+	public SuRenCompilerRequestor(String workDir)
 	{
-		// TODO Auto-generated method stub
-
+		this.workDir = workDir;
 	}
+	
+    public void acceptResult(CompilationResult result) {
+        if (result.hasErrors()) {
+            for (IProblem problem : result.getErrors()) {
+                String className = new String(problem.getOriginatingFileName()).replace("/", ".");
+                className = className.substring(0, className.length() - 5);
+                String message = problem.getMessage();
+                if (problem.getID() == IProblem.CannotImportPackage) {
+                    message = problem.getArguments()[0] + " cannot be resolved";
+                }
+                throw new RuntimeException(className + ":" + message);
+            }
+        }
 
+        ClassFile[] clazzFiles = result.getClassFiles();
+        for (int i = 0; i < clazzFiles.length; i++) {
+            String clazzName = join(clazzFiles[i].getCompoundName());
+            File target = new File(workDir, clazzName.replace(".", "/") + ".class");
+            try {
+                FileUtils.writeByteArrayToFile(target, clazzFiles[i].getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+	private String join(char[][] chars)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (char[] item : chars)
+		{
+			if (sb.length() > 0)
+			{
+				sb.append(".");
+			}
+			sb.append(item);
+		}
+		return sb.toString();
+	}
 }

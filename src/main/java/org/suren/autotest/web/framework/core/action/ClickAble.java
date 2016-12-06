b842bf5d626352a9102f187c2e774228f57ce7c3
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-12-06 00:12:50 +0000

    补充接口注释

--- src/main/java/org/suren/autotest/web/framework/core/action/ClickAble.java
+++ src/main/java/org/suren/autotest/web/framework/core/action/ClickAble.java
@@ -1,6 +1,3 @@
-/**
-* Copyright © 1998-2016, Glodon Inc. All Rights Reserved.
-*/
 package org.suren.autotest.web.framework.core.action;
 
 import org.suren.autotest.web.framework.core.ui.Element;
@@ -9,7 +6,7 @@ import org.suren.autotest.web.framework.core.ui.Element;
  * 可点击的接口行为，包括：单击、双击等
  * 
  * @author suren
- * @since jdk1.6 2016年6月29日
+ * @since jdk1.7 2016年6月29日
  */
 public interface ClickAble extends Status
 {
--- src/main/java/org/suren/autotest/web/framework/core/action/ValueEditor.java
+++ src/main/java/org/suren/autotest/web/framework/core/action/ValueEditor.java
@@ -1,6 +1,3 @@
-/**
-* Copyright © 1998-2016, Glodon Inc. All Rights Reserved.
-*/
 package org.suren.autotest.web.framework.core.action;
 
 import org.suren.autotest.web.framework.core.ui.Element;
@@ -9,7 +6,7 @@ import org.suren.autotest.web.framework.core.ui.Element;
  * 用于给文本框设置值、获取值的行为接口
  * 
  * @author suren
- * @since jdk1.6 2016年6月29日
+ * @since jdk1.7 2016年6月29日
  */
 public interface ValueEditor extends Status
 {

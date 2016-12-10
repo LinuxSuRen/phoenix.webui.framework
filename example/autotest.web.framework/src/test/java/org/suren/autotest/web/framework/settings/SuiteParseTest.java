5e950d6272fa41da3862db528dcc90207a11870c
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-12-10 14:44:04 +0000

    测试代码

--- example/autotest.web.framework/src/test/java/org/suren/autotest/web/framework/settings/SuiteParseTest.java
+++ example/autotest.web.framework/src/test/java/org/suren/autotest/web/framework/settings/SuiteParseTest.java
@@ -3,13 +3,15 @@
  */
 package org.suren.autotest.web.framework.settings;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.dom4j.DocumentException;
 import org.junit.Test;
 import org.suren.autotest.web.framework.core.suite.Suite;
+import org.suren.autotest.web.framework.validation.Validation;
+import org.xml.sax.SAXException;
 
 /**
  * 测试套件解析类测试
@@ -22,7 +24,15 @@ public class SuiteParseTest
 	public void test() throws FileNotFoundException, DocumentException
 	{
 		SuiteParser suiteParser = new SuiteParser();
-		Suite suite = suiteParser.parse(new FileInputStream(new File("D:/Program Files (x86)/Gboat-Toolkit-Suit/workspace_surenpi/autotest.web.framework/src/test/resources/runner_suite.xml")));
+		InputStream runnerInput = SuiteParseTest.class.getClassLoader().getResourceAsStream("runner_suite.xml");
+		Suite suite = suiteParser.parse(runnerInput);
 		System.out.println(suite);
+	}
+	
+	@Test
+	public void valid() throws SAXException, IOException
+	{
+		InputStream runnerInput = SuiteParseTest.class.getClassLoader().getResourceAsStream("runner_suite.xml");
+		Validation.validationSuite(runnerInput);
 	}
 }

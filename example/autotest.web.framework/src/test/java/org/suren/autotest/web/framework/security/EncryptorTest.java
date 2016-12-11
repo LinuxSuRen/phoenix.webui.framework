9b73f98618121ccd982f1832bb6b930b50005933
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-12-11 13:21:14 +0000

    增加加密类的单元测试

---
+++ example/autotest.web.framework/src/test/java/org/suren/autotest/web/framework/security/EncryptorTest.java
@@ -0,0 +1,36 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.security;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.junit.Test;
+
+/**
+ * @author suren
+ * @date 2016年12月11日 下午9:15:21
+ */
+public class EncryptorTest
+{
+	@Test
+	public void test() throws NoSuchAlgorithmException, NoSuchPaddingException, 
+		InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+	{
+		Encryptor encryptor = Encryptor.getInstance(Encryptor.ALG_DES);
+		
+		String plainText = "123456";
+		
+		String encryptText = encryptor.encryptStr(plainText);
+		System.out.println("plainText : " + plainText);
+		System.out.println("encryptText : " + encryptText);
+		
+		String decryptText = encryptor.decryptStr(encryptText);
+		System.out.println("decryptText : " + decryptText);
+	}
+}

9822a6c8b6fcb1796a53a0606badff37d577b6c8
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-12-29 23:54:22 +0000

    增加根据value选择的接口

--- src/main/java/org/suren/autotest/web/framework/core/action/CheckAble.java
+++ src/main/java/org/suren/autotest/web/framework/core/action/CheckAble.java
@@ -6,6 +6,7 @@ package org.suren.autotest.web.framework.core.action;
 import org.suren.autotest.web.framework.core.ui.Element;
 
 /**
+ * 可选择的行为动作
  * @author suren
  * @date Jul 27, 2016 12:56:46 PM
  */
@@ -13,7 +14,15 @@ public interface CheckAble
 {
 	/**
 	 * 根据文本来选择
+	 * @param element
 	 * @param text
 	 */
 	void checkByText(Element element, String text);
+	
+	/**
+	 * 根据值来选择
+	 * @param element
+	 * @param value
+	 */
+	void checkByValue(Element element, String value);
 }

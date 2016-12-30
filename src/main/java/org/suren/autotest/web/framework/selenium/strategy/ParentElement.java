9d8aca043718708d374a5d07ed7b63a6a12e071c
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-12-30 12:17:14 +0000

    增加可以设置元素查找的父的接口

---
+++ src/main/java/org/suren/autotest/web/framework/selenium/strategy/ParentElement.java
@@ -0,0 +1,15 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.selenium.strategy;
+
+import org.openqa.selenium.WebElement;
+
+/**
+ * @author suren
+ * @date 2016年12月30日 下午7:48:26
+ */
+public interface ParentElement
+{
+	void setParent(WebElement parentWebElement);
+}

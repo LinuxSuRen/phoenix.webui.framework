37edb5e5a1b669c09cd101efaec7dedd5df7579a
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2017-01-04 04:37:49 +0000

    增加动态数据的接口

---
+++ src/main/java/org/suren/autotest/web/framework/data/DynamicData.java
@@ -0,0 +1,14 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.data;
+
+/**
+ * 动态数据
+ * @author suren
+ * @date 2017年1月4日 下午12:30:53
+ */
+public interface DynamicData
+{
+	String getValue(String orginData);
+}

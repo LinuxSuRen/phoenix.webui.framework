82dc15cfb38139292d82f95d90f7e9ca76a5f417
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2017-01-11 13:51:01 +0000

    增加序列操作接口

---
+++ src/main/java/org/suren/autotest/web/framework/core/action/SequenceAble.java
@@ -0,0 +1,23 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.core.action;
+
+import java.util.List;
+
+import org.suren.autotest.web.framework.core.ui.Element;
+
+/**
+ * 序列操作
+ * @author suren
+ * @date 2017年1月11日 下午4:53:00
+ */
+public interface SequenceAble
+{
+	/**
+	 * 执行序列操作
+	 * @param element 触发该操作的元素
+	 * @param actions 序列操作信息
+	 */
+	void perform(Element element, List<String> actions);
+}

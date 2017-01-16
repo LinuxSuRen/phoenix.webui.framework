853f07ace1fefda1512adfa4f223456ba150861a
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2017-01-16 00:15:04 +0000

    增加随机创建文件的接口

---
+++ example/autotest.web.framework/src/main/java/org/suren/autotest/web/framework/core/RandomFile.java
@@ -0,0 +1,19 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.core;
+
+import java.io.File;
+
+/**
+ * 创建一个随机文件
+ * @author suren
+ * @date 2017年1月15日 上午10:05:39
+ */
+public interface RandomFile
+{
+	/**
+	 * @return 随机文件
+	 */
+	File createFile();
+}

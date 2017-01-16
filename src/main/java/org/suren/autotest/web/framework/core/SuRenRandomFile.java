8b9351580d954496c679b195037158d963f13bc3
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2017-01-16 00:15:32 +0000

    增加随机创建文件的实现类

---
+++ src/main/java/org/suren/autotest/web/framework/core/SuRenRandomFile.java
@@ -0,0 +1,45 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.web.framework.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.suren.autotest.web.framework.selenium.SeleniumEngine;
+import org.suren.autotest.web.framework.util.PathUtil;
+
+/**
+ * 默认提供的随机文件生成类
+ * @author suren
+ * @date 2017年1月15日 上午10:06:31
+ */
+@Component
+public class SuRenRandomFile implements RandomFile
+{
+	@Autowired
+	private SeleniumEngine			engine;
+	@Autowired
+	private RandomFileContent randomContent;
+
+	@Override
+	public File createFile()
+	{
+		String fileName = (String) engine.getEngineConfig().get("upload.file.random.name");
+		File randomFile = null;
+		try(InputStream content = randomContent.getContent())
+		{
+			randomFile = PathUtil.copyFileToRoot(content, fileName);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return randomFile;
+	}
+
+}

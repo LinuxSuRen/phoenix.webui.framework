4e3db7b2b8f3837aa6616042ee602c09a84bee21
surenpi <zxjlwt@126.com>
2017-02-20 21:29:45 +0800

    解析浏览器和驱动匹配的配置文件

---
+++ src/main/java/org/suren/autotest/web/framework/selenium/DriverMapping.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.suren.autotest.web.framework.selenium;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.XPath;
+import org.dom4j.io.SAXReader;
+import org.dom4j.xpath.DefaultXPath;
+
+/**
+ * 浏览器和驱动对应关系
+ * @author suren
+ * @date 2017年2月20日 下午3:52:07
+ */
+public class DriverMapping
+{
+	private Document document;
+
+	/**
+	 * 读取配置
+	 */
+	public void init()
+	{
+		try(InputStream input = this.getClass().getClassLoader().getResourceAsStream("driver.mapping.xml");
+				InputStream test = this.getClass().getClassLoader().getResourceAsStream("engine.properties"))
+		{
+			SAXReader reader = new SAXReader();
+			
+			document = reader.read(input);
+			
+			Properties pro = new Properties();
+			pro.load(test);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (DocumentException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public String getUrl(String browser, String ver)
+	{
+		return getUrl(browser, ver, "win32", "32");
+	}
+	
+	/**
+	 * @param browser 浏览器类型
+	 * @param ver 浏览器版本
+	 * @param os 操作系统
+	 * @param arch 系统架构
+	 * @return 对应的驱动远程下载地址
+	 */
+	public String getUrl(String browser, String ver, String os, String arch)
+	{
+		String xpathStr = String.format("//drivers/driver[@type='%s']/supports/browser[@version='%s']",
+				browser, ver);
+		XPath xpath = new DefaultXPath(xpathStr);
+
+		String path = null;
+		List<Element> nodes = xpath.selectNodes(document);
+		for(Element ele : nodes)
+		{
+			List<Element> itemList = ele.getParent().getParent().element("items").elements("item");
+			for(Element item : itemList)
+			{
+				if(os.equals(item.attributeValue("os")) && arch.equals(item.attributeValue("arch")))
+				{
+					path = item.attributeValue("path");
+					break;
+				}
+			}
+			
+			if(path != null)
+			{
+				break;
+			}
+		}
+		
+		if(path != null)
+		{
+			String base = document.getRootElement().attributeValue("base");
+			path = base + path;
+		}
+		
+		return path;
+	}
+	
+	public static void main(String[] args)
+	{
+		DriverMapping driverMapping = new DriverMapping();
+		driverMapping.init();
+		
+		String url = driverMapping.getUrl("chrome", "56");
+		
+		System.out.println(url);
+		
+		System.out.println(System.getProperty("os.arch"));
+	}
+}

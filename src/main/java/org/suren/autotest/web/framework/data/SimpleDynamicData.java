1d60d2f83691c47db1bfc6d266e79af533ef4af2
surenpi <zxjlwt@126.com>
2017-03-25 10:49:12 +0800

    修正有关简单数据的日期部分

--- src/main/java/org/suren/autotest/web/framework/data/SimpleDynamicData.java
+++ src/main/java/org/suren/autotest/web/framework/data/SimpleDynamicData.java
@@ -1,13 +1,27 @@
-/**
- * http://surenpi.com
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.suren.autotest.web.framework.data;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.stereotype.Component;
 import org.suren.autotest.web.framework.util.CommonNumberUtil;
@@ -21,16 +35,19 @@ import org.suren.autotest.web.framework.util.StringUtils;
  * @date 2017年1月4日 下午12:33:01
  */
 @Component
-public class SimpleDynamicData implements DynamicData
+public class SimpleDynamicData implements DynamicData, DynamicDateFormat
 {
-	private List<String> formatList = new ArrayList<String>();
-	
+	private Set<String> formatSet = new HashSet<String>();
+
+	private SimpleDateFormat dateFormat = new SimpleDateFormat();
 	private String random = "${random-";
 	
 	public SimpleDynamicData()
 	{
-		formatList.add("yyyy-MM-dd");
-		formatList.add("yyyy-MM-dd HH:mm:ss");
+		formatSet.add("yyyy");
+		formatSet.add("MM-dd");
+		formatSet.add("yyyy-MM-dd");
+		formatSet.add("yyyy-MM-dd HH:mm:ss");
 	}
 
 	@Override
@@ -38,14 +55,16 @@ public class SimpleDynamicData implements DynamicData
 	{
 		String value = orginData;
 		
+		Date nowDate = new Date();
 		value = value.replace("${now}", String.valueOf(System.currentTimeMillis()));
 		
-		for(String format : formatList)
+		for(String format : formatSet)
 		{
 			String param = "${now " + format + "}";
 			if(value.contains(param))
 			{
-				value = value.replace(param, new SimpleDateFormat(format).format(new Date()));
+				dateFormat.applyPattern(format);
+				value = value.replace(param, dateFormat.format(nowDate));
 			}
 		}
 		
@@ -107,6 +126,28 @@ public class SimpleDynamicData implements DynamicData
 	@Override
 	public void setData(Map<String, Object> data)
 	{
+	}
+
+	@Override
+	public Set<String> allFormats()
+	{
+		return Collections.unmodifiableSet(formatSet);
+	}
+
+	@Override
+	public boolean addFormat(String format)
+	{
+		try
+		{
+			dateFormat.applyPattern(format);
+			formatSet.add(format);
+			return true;
+		}
+		catch(IllegalArgumentException | NullPointerException e)
+		{
+		}
+		
+		return false;
 	}
 
 }

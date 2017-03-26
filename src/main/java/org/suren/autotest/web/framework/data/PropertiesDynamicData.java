e43f1d6023c0c5a45ebb20c23fa72fe80a9fb8a0
surenpi <zxjlwt@126.com>
2017-03-26 16:17:56 +0800

    增加属性文件类型的参数化实现

---
+++ src/main/java/org/suren/autotest/web/framework/data/PropertiesDynamicData.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.suren.autotest.web.framework.data;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 属性文件动态数据
+ * @author suren
+ * @date 2017年3月26日 下午12:52:28
+ */
+@Component
+public class PropertiesDynamicData implements DynamicData
+{
+	private String path = "dynamic.data.properties";
+	private Properties pro = new Properties();
+
+	@Override
+	public String getValue(final String orginData)
+	{
+		String data = orginData;
+		for(Object key : pro.keySet())
+		{
+			String strKey = key.toString();
+			String pattern = ("${" + strKey + "}");
+			
+			data = data.replace(pattern, pro.getProperty(strKey));
+		}
+		
+		return data;
+	}
+
+	@Override
+	public String getType()
+	{
+		return "properties";
+	}
+
+	@Override
+	public void setData(Map<String, Object> data)
+	{
+	}
+
+	@PostConstruct
+	public void init() throws IOException
+	{
+		ClassLoader loader = PropertiesDynamicData.class.getClassLoader();
+		try(InputStream input = loader.getResourceAsStream(path))
+		{
+			pro.load(input);
+		}
+	}
+
+	public String getPath()
+	{
+		return path;
+	}
+
+	public void setPath(String path)
+	{
+		this.path = path;
+	}
+
+}

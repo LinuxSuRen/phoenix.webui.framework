2a11e6f222adae669242662c2a936c88020b3406
surenpi <zxjlwt@126.com>
2017-03-31 22:53:17 +0800

    代码整理

--- src/main/java/org/suren/autotest/web/framework/invoker/KaptchaInvoker.java
+++ src/main/java/org/suren/autotest/web/framework/invoker/KaptchaInvoker.java
@@ -1,6 +1,19 @@
-/**
- * http://surenpi.com
-*/
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.suren.autotest.web.framework.invoker;
 
 import java.util.ArrayList;
@@ -15,6 +28,7 @@ import org.suren.autotest.interfaces.framework.param.AtCookie;
 import org.suren.autotest.web.framework.selenium.SeleniumEngine;
 
 /**
+ * 验证码外部执行器
  * @author suren
  * @date 2017年1月7日 下午9:21:14
  */
@@ -25,7 +39,8 @@ public class KaptchaInvoker {
 	 * @param param 例如：data,http://localhost:8080/G2/captcha!getLastCode.do
 	 * @return
 	 */
-	public static String execute(SeleniumEngine engine, String param) {
+	public static String execute(SeleniumEngine engine, String param)
+	{
 		WebDriver driver = engine.getDriver();
 		Options manage = driver.manage();
 		
@@ -41,7 +56,8 @@ public class KaptchaInvoker {
 		
 		Set<Cookie> cookies = manage.getCookies();
 		List<AtCookie> atCookieList = new ArrayList<AtCookie>();
-		for(Cookie cookie : cookies) {
+		for(Cookie cookie : cookies)
+		{
 			String name = cookie.getName();
 			String value = cookie.getValue();
 			

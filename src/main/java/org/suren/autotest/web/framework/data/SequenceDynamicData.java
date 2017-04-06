08cf7907986257a78738e8ab474745b8a970aefe
surenpi <zxjlwt@126.com>
2017-04-06 21:39:38 +0800

    修改序列化操作参数的实现

--- src/main/java/org/suren/autotest/web/framework/data/SequenceDynamicData.java
+++ src/main/java/org/suren/autotest/web/framework/data/SequenceDynamicData.java
@@ -1,9 +1,20 @@
-/**
- * http://surenpi.com
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package org.suren.autotest.web.framework.data;
 
-import java.util.Map;
+package org.suren.autotest.web.framework.data;
 
 import org.springframework.stereotype.Component;
 
@@ -12,24 +23,13 @@ import org.springframework.stereotype.Component;
  * @date 2017年1月11日 下午8:40:18
  */
 @Component
-public class SequenceDynamicData implements DynamicData
+public class SequenceDynamicData extends CallbackDynamicData implements DynamicData
 {
-
-	@Override
-	public String getValue(String orginData)
-	{
-		return orginData;
-	}
 
 	@Override
 	public String getType()
 	{
 		return "sequence";
-	}
-
-	@Override
-	public void setData(Map<String, Object> data)
-	{
 	}
 
 }

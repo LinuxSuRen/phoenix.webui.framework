2fc90b59ec3f7652c19e0d2717eeb90546f7f364
surenpi <zxjlwt@126.com>
2017-05-13 21:50:03 +0800

    修改加密工具类的测试

--- src/test/java/org/suren/autotest/web/framework/util/EncryptUtilTest.java
+++ src/test/java/org/suren/autotest/web/framework/util/EncryptUtilTest.java
@@ -1,9 +1,21 @@
-/**
- * http://surenpi.com
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.suren.autotest.web.framework.util;
 
-import org.apache.commons.codec.binary.Base64;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,11 +34,5 @@ public class EncryptUtilTest
 		String afterPlainText = EncryptorUtil.decrypt(encryptText);
 		
 		Assert.assertEquals(plainText, afterPlainText);
-		
-		System.out.println(encryptText);
-		
-		String base64 = Base64.encodeBase64String(encryptText.getBytes());
-		System.out.println(base64);
-		System.out.println(new String(Base64.decodeBase64(base64.getBytes())));
 	}
 }

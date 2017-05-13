2a7592ea9ed783f33149f5fdd8eba37121b846b0
surenpi <zxjlwt@126.com>
2017-05-13 18:50:08 +0800

    添加注释

--- src/test/java/org/suren/autotest/web/framework/util/SettingUtilTest.java
+++ src/test/java/org/suren/autotest/web/framework/util/SettingUtilTest.java
@@ -19,45 +19,115 @@ package org.suren.autotest.web.framework.util;
 import java.io.IOException;
 
 import org.dom4j.DocumentException;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.suren.autotest.web.framework.core.ui.Button;
 import org.suren.autotest.web.framework.core.ui.Text;
 import org.suren.autotest.web.framework.page.HomePage;
 import org.suren.autotest.web.framework.page.LoginPage;
+import org.suren.autotest.web.framework.page.MenuPage;
+import org.suren.autotest.web.framework.page.SSIPPage;
 import org.suren.autotest.web.framework.settings.SettingUtil;
 import org.xml.sax.SAXException;
 
 /**
+ * 关于框架加载元素定位和数据部分的单元测试
  * @author suren
  * @date 2017年5月13日 下午12:06:10
  */
 public class SettingUtilTest
 {
+	private SettingUtil util;
+	
+	/**
+	 * 加载元素定位信息，初始化数据源
+	 * @throws IOException
+	 * @throws DocumentException
+	 * @throws SAXException
+	 */
+	@Before
+	public void before() throws IOException, DocumentException, SAXException
+	{
+		util = new SettingUtil();
+		util.readFromClassPath("elements/xml/maimai.xml");
+		util.initData();
+	}
+	
+	/**
+	 * 释放资源
+	 * @throws IOException
+	 */
+	@After
+	public void after() throws IOException
+	{
+		util.close();
+	}
+
+	/**
+	 * 测试Page类加载
+	 */
+	@Test
+	public void pageLoad()
+	{
+		Assert.assertNotNull(util.getPage(HomePage.class));
+		Assert.assertNotNull(util.getPage(LoginPage.class));
+		Assert.assertNotNull(util.getPage(MenuPage.class));
+		Assert.assertNotNull(util.getPage(SSIPPage.class));
+	}
+	
+	/**
+	 * 测试简单流程，通过编码的方式输入数据
+	 * @throws IOException
+	 * @throws DocumentException
+	 * @throws SAXException
+	 */
 	@Test
 	public void simple() throws IOException, DocumentException, SAXException
 	{
-		try(SettingUtil util = new SettingUtil())
-		{
-			util.readFromClassPath("elements/xml/maimai.xml");
-			util.initData();
-			
-			HomePage homePage = util.getPage(HomePage.class);
-			Assert.assertNotNull(homePage);
+		HomePage homePage = util.getPage(HomePage.class);
+		Assert.assertNotNull(homePage);
 
-			Assert.assertNotNull(homePage.getUrl());
-			Assert.assertTrue("起始页地址不合法", homePage.paramTranslate(homePage.getUrl()).startsWith("http"));
-			homePage.open();
-			Button toLoginBut = homePage.getToLoginBut();
-			Assert.assertNotNull(toLoginBut);
-			toLoginBut.click();
-			
-			LoginPage loginPage = util.getPage(LoginPage.class);
-			Assert.assertNotNull(loginPage);
-			
-			Text phone = loginPage.getPhone();
-			Assert.assertNotNull(phone);
-			phone.fillNotBlankValue();
-		}
+		Assert.assertNotNull(homePage.getUrl());
+		Assert.assertTrue("起始页地址不合法",
+				homePage.paramTranslate(homePage.getUrl()).startsWith("http"));
+		homePage.open();
+		Button toLoginBut = homePage.getToLoginBut();
+		Assert.assertNotNull(toLoginBut);
+		toLoginBut.click();
+		
+		LoginPage loginPage = util.getPage(LoginPage.class);
+		Assert.assertNotNull(loginPage);
+		
+		Text phone = loginPage.getPhone();
+		Assert.assertNotNull(phone);
+		phone.fillValue("18211145623");
+	}
+
+	/**
+	 * 测试简单流程，数据在数据源中
+	 */
+	@Test
+	public void dataSource()
+	{
+		HomePage homePage = util.getPage(HomePage.class);
+		Assert.assertNotNull(homePage);
+
+		Assert.assertNotNull(homePage.getUrl());
+		Assert.assertTrue("起始页地址不合法",
+				homePage.paramTranslate(homePage.getUrl()).startsWith("http"));
+		homePage.open();
+		Button toLoginBut = homePage.getToLoginBut();
+		Assert.assertNotNull(toLoginBut);
+		toLoginBut.click();
+		
+		LoginPage loginPage = util.getPage(LoginPage.class);
+		Assert.assertNotNull(loginPage);
+		
+		Text phone = loginPage.getPhone();
+		Assert.assertNotNull(phone);
+		Assert.assertNotNull("数据未从数据源中加载", phone.getValue());
+		phone.fillNotBlankValue();
 	}
 }

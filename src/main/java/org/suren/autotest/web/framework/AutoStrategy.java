7a88a91a9e99184b8b5f5783eb82a44a7fd60a5e
surenpi <zxjlwt@126.com>
2017-06-07 19:09:11 +0800

    增加基本的有关元素定位的注解

---
+++ src/main/java/org/suren/autotest/web/framework/AutoStrategy.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.suren.autotest.web.framework.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import org.suren.autotest.web.framework.core.StrategyType;
+
+/**
+ * 定位策略
+ * @author suren
+ * @date 2017年6月7日 下午7:03:57
+ */
+@Target(value = ElementType.FIELD)
+@Documented
+public @interface AutoStrategy
+{
+	/**
+	 * 默认是优先级策略
+	 * @return 定位策略类型
+	 */
+	StrategyType type() default StrategyType.PRIORITY;
+}

ffcbf93844ed815ae496133c14c40cab7d5c6e82
surenpi <zxjlwt@126.com>
2017-06-09 20:16:00 +0800

    断言类使用错误

--- src/test/java/org/suren/autotest/web/framework/xsd/Xsd.java
+++ src/test/java/org/suren/autotest/web/framework/xsd/Xsd.java
@@ -1,34 +1,35 @@
 /*
- * Copyright 2002-2007 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2002-2007 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package org.suren.autotest.web.framework.xsd;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
-import javax.xml.transform.stream.StreamSource;
 
 import org.apache.ws.commons.schema.XmlSchema;
 import org.apache.ws.commons.schema.XmlSchemaCollection;
 import org.apache.ws.commons.schema.XmlSchemaElement;
 import org.apache.ws.commons.schema.XmlSchemaObject;
-import org.eclipse.core.runtime.Assert;
+import org.junit.Assert;
 import org.junit.Test;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * @author suren
@@ -41,7 +42,7 @@ public class Xsd
 	{
 		try(InputStream input = Xsd.class.getResourceAsStream("/autotest.web.framework.xsd"))
 		{
-			Assert.isNotNull(input);
+			Assert.assertNotNull(input);
 			
 			XmlSchemaCollection schemaCol = new XmlSchemaCollection();
 			XmlSchema schema = schemaCol.read(new StreamSource(input));

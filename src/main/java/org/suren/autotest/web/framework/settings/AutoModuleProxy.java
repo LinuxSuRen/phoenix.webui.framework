ae29a6ac939bb6211ad8b21df676a8ea50cb5a91
surenpi.com <LinuxSuRen@gmail.com>
2017-06-13 22:30:47 +0800

    增加动态代理

---
+++ src/main/java/org/suren/autotest/web/framework/settings/AutoModuleProxy.java
@@ -0,0 +1,49 @@
+/*
+ *
+ *  * Copyright 2002-2007 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.suren.autotest.web.framework.settings;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author suren
+ */
+public class AutoModuleProxy implements MethodInterceptor
+{
+    private Enhancer enhancer = new Enhancer();
+
+    public Object getProxy(Class<?> clazz)
+    {
+        enhancer.setSuperclass(clazz);
+        enhancer.setCallback(this);
+        return enhancer.create();
+    }
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args,
+                            MethodProxy methodProxy) throws Throwable
+    {
+        System.out.println("auto model pre method");
+        Object result = methodProxy.invokeSuper(obj, args);
+        return result;
+    }
+}
---
+++ src/main/java/org/suren/autotest/web/framework/spring/AutoModuleScope.java
@@ -0,0 +1,74 @@
+/*
+ *
+ *  * Copyright 2002-2007 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.suren.autotest.web.framework.spring;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.suren.autotest.web.framework.settings.AutoModuleProxy;
+
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author suren
+ */
+public class AutoModuleScope implements Scope
+{
+    private Map<String, Object> objMap = new HashMap<String, Object>();
+
+    @Override
+    public Object get(String name, ObjectFactory<?> objectFactory)
+    {
+        Object object = objMap.get(name);
+        if(object == null)
+        {
+            object = objectFactory.getObject();
+            AutoModuleProxy proxy = new AutoModuleProxy();
+            object = proxy.getProxy(object.getClass());
+            objMap.put(name, object);
+        }
+
+        return object;
+    }
+
+    @Override
+    public Object remove(String name)
+    {
+        return objMap.remove(name);
+    }
+
+    @Override
+    public void registerDestructionCallback(String name, Runnable callback)
+    {
+    }
+
+    @Override
+    public Object resolveContextualObject(String key)
+    {
+        return null;
+    }
+
+    @Override
+    public String getConversationId()
+    {
+        return null;
+    }
+}

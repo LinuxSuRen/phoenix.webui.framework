97015b3137adf716ec2552e34179fb16b4179a78
surenpi.com <LinuxSuRen@gmail.com>
2017-06-16 23:02:09 +0800

    把该实现设置为首要选择

--- src/main/java/org/suren/autotest/web/framework/log/LoggerRecordReport.java
+++ src/main/java/org/suren/autotest/web/framework/log/LoggerRecordReport.java
@@ -18,6 +18,7 @@ package org.suren.autotest.web.framework.log;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.suren.autotest.web.framework.report.RecordReportWriter;
 import org.suren.autotest.web.framework.report.record.ExceptionRecord;
@@ -29,6 +30,7 @@ import org.suren.autotest.web.framework.report.record.ProjectRecord;
  * @author suren
  * @date 2016年9月6日 下午9:10:47
  */
+@Primary
 @Component
 public class LoggerRecordReport implements RecordReportWriter
 {

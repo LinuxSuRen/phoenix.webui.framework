8963d0b47a42d48f79da22fb001ae8e8b75ce9df
suren <zxjlwt@126.com>
2017-06-17 16:06:30 +0800

    增加自定义信息

--- src/main/java/org/suren/autotest/web/framework/annotation/AutoApplication.java
+++ src/main/java/org/suren/autotest/web/framework/annotation/AutoApplication.java
@@ -30,6 +30,13 @@ public @interface AutoApplication
     String description() default "";
 
     /**
+     * 该信息会在测试报告中体现。支持的文件格式为：.yml、.properties、.txt等。</br>
+     * ymal和properties文件都会被解析成成json字符串。
+     * @return 用户自定义的环境信息，classpath下的路径
+     */
+    String customEnvInfo() default "";
+
+    /**
      * @return 项目（模块）关注者的邮件列表
      */
     String[] concernMailList() default {};

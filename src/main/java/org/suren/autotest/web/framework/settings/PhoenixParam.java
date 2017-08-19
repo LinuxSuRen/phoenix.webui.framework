0764d187bbe8091bece581daeff2cf06217faf06
surenpi <zxjlwt@126.com>
2017-08-19 20:45:24 +0800

    add param

--- src/main/java/org/suren/autotest/web/framework/settings/PhoenixParam.java
+++ src/main/java/org/suren/autotest/web/framework/settings/PhoenixParam.java
@@ -43,11 +43,31 @@ public class PhoenixParam
     
     @Parameter(names = "-remote", description = "远程服务器地址，例如：http://192.168.1.1:1234/wb/hub")
     public String remote;
+    
+    @Parameter(names = {"-remote-ws", "-remote-workspace"},
+            description = "远程服务器工作目录")
+    public String remoteWorkspace;
+    
+    @Parameter(names = {"-remote-p", "-remote-protocol"},
+            description = "远程服务器协议，支持的协议：ssh")
+    public String remoteProtocol;
+
+    @Parameter(names = "-ssh-host", description = "ssh地址")
+    public String sshHost;
+
+    @Parameter(names = "-ssh-port", description = "ssh端口")
+    public int sshPort = 22;
+    
+    @Parameter(names = "-ssh-user", description = "ssh用户名")
+    public String sshUser;
+    
+    @Parameter(names = "-ssh-password", password = true, echoInput = true, description = "ssh密码")
+    public String sshPasswd;
 
     @Parameter(names = "-report-store", description = "测试报告输出目录，只适用于有文件输出的报告类型",
             validateWith = {FolderValidator.class})
     public String reportStore;
 
-    @Parameter(names = "-printUsage", description = "打印帮助信息并退出程序")
+    @Parameter(names = {"-printUsage", "-help"}, description = "打印帮助信息并退出程序")
     public boolean printUsage;
 }
